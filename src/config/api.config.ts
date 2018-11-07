f4a939dbd1f1a04359e2b2544322bc7e82b4db01
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-11-07 17:04:25 -0200

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://jdc-loja.herokuapp.com",
+    //DEV baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/jdc-loja/"
 }
