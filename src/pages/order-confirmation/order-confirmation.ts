0a2a0083bfd6a78e4e0d85190344d356e345a5f5
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-11-06 22:32:22 -0200

    Informando o pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -54,14 +55,23 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedServ.insert(this.pedido).subscribe(response => {
       this.cartServ.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
     }, error => {
       if (error.status == 403) {
         this.navCtrl.setRoot('HomePage');
       }
     });
+  }
+
+  private extractId(location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position +1, location.length);
   }
 }
